b66cf08e437c2ba8a8a7cbf493e45a886b84e9c3
Henrique <hiquesc_@hotmail.com>
2020-07-22 23:02:49 -0300

    CRUD React

---
+++ README.md
@@ -0,0 +1,18 @@
+# CRUD - React
+
+CRUD básico desenvolvido para aprendizado do React
+
+> React de leves
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm start
+
+# build for production with minification
+npm build
+```
---
+++ frontend/src/components/templates/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+  <footer className="footer">
+    <span>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+      <strong> HC<span className="text-danger"> Soluções WEB</span></strong>
+    </span>
+  </footer>
